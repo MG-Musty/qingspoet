afc9edc341a61b7681f72699261b75d8046d06a4
MG-Musty <mustaphaaliyugaladima@gmail.com>
2022-12-15 17:42:56 +0100

    build files

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-pip install -r requirements/production.txt
+pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic
 python manage.py startapp qings
-django-admin startproject qingspoet
