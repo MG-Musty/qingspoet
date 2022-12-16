c795964effbf16eaa8d569741f426362a3ddec16
MG-Musty <mustaphaaliyugaladima@gmail.com>
2022-12-16 20:15:11 +0100

    New updated files

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+set -o errexit  # exit on error
+
 pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py collectstatic
-python manage.py startapp qings
